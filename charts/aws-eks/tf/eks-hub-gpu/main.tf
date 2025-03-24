7c559fe4deeb1b798b0c07769e68a1ce2da56a0b
Michael Pulido <mike@nethopper.io>
2025-03-24 08:37:18 -0400

    fix: removing token from terraform block

--- charts/aws-eks/tf/eks-hub-gpu/main.tf
+++ charts/aws-eks/tf/eks-hub-gpu/main.tf
@@ -4,14 +4,12 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    token                  = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   }
 }
