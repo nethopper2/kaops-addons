590aa0e2863861824244aa8ab97a4f40727ddb0c
Kamil Litwiniuk <kamil.litwiniuk@acaisoft.com>
2025-03-27 18:51:21 +0100

    fix(aws-eks): add missing data object

--- charts/aws-eks/tf/eks-hub-gpu/main.tf
+++ charts/aws-eks/tf/eks-hub-gpu/main.tf
@@ -8,6 +8,8 @@ data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_name
 }
 
+data "aws_caller_identity" "current" {}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
